c9d85e9182b6b2d05b056915e4c6e25a8c6a3049
Ahmed Azeem <55770511+zimaxeg@users.noreply.github.com>
2020-07-24 06:18:33 +0200

    Update functions_lesson2_module6.py

--- functions_lesson2_module6.py
+++ functions_lesson2_module6.py
@@ -45,8 +45,10 @@ create_spreadsheet("Applications")
 def calculate_age(current_year, birth_year):
   age = current_year - birth_year
   return age
-my_age=calculate_age(2049,1993)
-dads_age=calculate_age(2049,1953)
+  
+my_age = calculate_age(2049, 1993)
+dads_age = calculate_age(2049, 1953)
+print("I am "+str(my_age)+" years old and my dad is "+str(dads_age)+" years old")
 
 def get_boundaries(target,margin):
   low_limit=target-margin
